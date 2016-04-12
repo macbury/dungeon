476995bd8048f4ea71b25f81aad51d2d5e02d6c6
Arkadiusz Buras <macbury@gmail.com>
2016-04-12 13:23:38 +0200

    Initial commit

---
+++ src/boot.ts
@@ -0,0 +1,14 @@
+/// <reference path="./defs/definitions.d.ts"/>
+import 'p2';
+import 'pixi.js';
+import { Game } from 'phaser';
+
+class Test {
+  constructor() {
+    console.log("Test2");
+
+  }
+}
+
+new Test();
+console.log("AS");
---
+++ src/defs/definitions.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../node_modules/phaser/typescript/phaser.comments.d.ts"/>
+/// <reference path="../../node_modules/phaser/typescript/pixi.comments.d.ts"/>
+/// <reference path="../../node_modules/phaser/typescript/p2.d.ts"/>
