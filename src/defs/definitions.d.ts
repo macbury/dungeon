b84dce38c8ad67af918aab2f9ab5c6f87ae0bd21
Arkadiusz Buras <macbury@gmail.com>
2016-04-25 11:10:26 +0200

    Some trash removal

--- src/defs/definitions.d.ts
+++ src/defs/definitions.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../node_modules/phaser/typescript/phaser.comments.d.ts"/>
-/// <reference path="../../node_modules/phaser/typescript/pixi.comments.d.ts"/>
+// <reference path="../../node_modules/phaser/typescript/pixi.comments.d.ts"/>
 /// <reference path="../../node_modules/phaser/typescript/p2.d.ts"/>
 /**
 * With this we can require webpack modules in typescript
