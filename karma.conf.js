6bf6c028c4412e1dae65d600d4ae1984d16edbf5
Arkadiusz Buras <macbury@gmail.com>
2016-05-01 17:20:26 +0200

    Force karma js

---
+++ karma.conf.js
@@ -0,0 +1,75 @@
+// Karma configuration
+// Generated on Sun May 01 2016 12:45:45 GMT+0200 (CEST)
+var webpackConfig = require("./webpack.config.js");
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ["mocha", "chai", "sinon"],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+    plugins: [
+        "karma-*",
+        require("karma-webpack")
+    ],
+
+
+    preprocessors: {
+        "test/**/*.ts": ["webpack"] // Using karma-webpack npm module
+    },
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve
+    }
+  })
+}
