5a6c4a6c64d2131adf20eba7b7c419d5f3751435
Arkadiusz Buras <macbury@gmail.com>
2016-05-06 12:58:38 +0200

    Simple monster killing

---
+++ src/core/objects/pending_actions/pending_die_action.ts
@@ -0,0 +1,24 @@
+import { PendingTurnAction } from './pending_turn_actions';
+import Character from '../character';
+import NarrationManager from '../../narration_manager';
+
+export default class PendingDieAction extends PendingTurnAction<Character> {
+
+  protected performTurn() : void {
+    var tween : Phaser.Tween = this.add.tween(this.owner).to({
+      alpha: 0.0
+    }, 100);
+
+    tween.onComplete.addOnce(() => {
+      this.owner.destroy();
+      this.onCompleteSignal.dispatch();
+    });
+
+    tween.start();
+  }
+
+
+  public turnDescription(narration : NarrationManager) : void {
+    narration.info("Killed monster?");
+  }
+}
